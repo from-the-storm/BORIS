3a93b21f7b56ab824462fa219b66abf9fc83107f
Braden MacDonald <mail@bradenm.com>
2018-03-23 09:49:29 -0700

    Fix webpack URLs for large images

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: __dirname + "/dist",
+        publicPath: '/s/',
     },
 
     // Enable sourcemaps for debugging webpack's output.
