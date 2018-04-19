b400c9a6d695a6ad0b7a94c9ef358570ae7ba2a9
Braden MacDonald <mail@bradenm.com>
2018-04-19 16:41:26 -0700

    Test get_initial_state with an active team

--- backend/routes/app-api-test.ts
+++ backend/routes/app-api-test.ts
@@ -1,6 +1,6 @@
 import 'jest';
 import { TestClient, TestServer } from '../test-lib/utils';
-import { GET_INITIAL_STATE } from '../../common/api';
+import { GET_INITIAL_STATE, CREATE_TEAM } from '../../common/api';
 
 describe("App API tests", () => {
     describe("getOtherTeamMembers", async () => {
@@ -29,8 +29,18 @@ describe("App API tests", () => {
         it("Returns user info when a user is logged in", async () => {
             const userInfo = await client.registerAndLogin();
             const result = await client.callApi(GET_INITIAL_STATE, {});
-            expect(result.team).toBeUndefined();
             expect(result.user).toEqual({first_name: "Jamie"});
+            expect(result.team).toBeUndefined();
+        });
+        it("Returns user info when a user is logged and on a team", async () => {
+            const userInfo = await client.registerAndLogin();
+            await client.callApi(CREATE_TEAM, { teamName: "Test Team", organizationName: ""});
+            const result = await client.callApi(GET_INITIAL_STATE, {});
+            expect(result.user).toEqual({first_name: "Jamie"});
+            expect(result.team.name).toEqual("Test Team");
+            expect(result.team.code).toHaveLength(5);
+            expect(result.team.isTeamAdmin).toBe(true);
+            expect(result.team.otherTeamMembers).toEqual([]);
         });
     });
 });
