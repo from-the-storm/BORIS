3d01294d21acc1ebc65f9769744f062b0bb26c5d
Braden MacDonald <mail@bradenm.com>
2018-04-24 16:36:09 -0700

    Team page listing team members

--- frontend/lobby/lobby.tsx
+++ frontend/lobby/lobby.tsx
@@ -4,6 +4,7 @@ import {connect, DispatchProp} from 'react-redux';
 
 import {RootState} from '../global/state';
 import { ChooseScenarioComponent } from './choose-scenario';
+import { TeamComponent } from './team';
 import {Mode} from './lobby-state';
 import {Actions} from './lobby-state-actions';
 import { Actions as RegistrationActions } from '../registration/registration-state-actions';
@@ -28,9 +29,15 @@ class _LobbyComponent extends React.PureComponent<Props> {
     @bind private handleBackButton() {
         if (this.props.mode === Mode.ChooseScenario && this.props.viewingScenarioDetails) {
             this.props.dispatch<AnyAction>({type: Actions.SHOW_SCENARIOS_LIST});
+        } if (this.props.mode === Mode.TeamDetails) {
+            this.props.dispatch<AnyAction>({type: Actions.SHOW_SCENARIOS_LIST});
         } else {
             this.props.dispatch<AnyAction>({type: RegistrationActions.SHOW_HOME});
         }
+    }
+
+    @bind private handleTeamButton() {
+        this.props.dispatch<AnyAction>({type: Actions.SHOW_TEAM_DETAILS});
     }
 
     public render() {
@@ -38,10 +45,12 @@ class _LobbyComponent extends React.PureComponent<Props> {
             <div className="lobby">
                 <header>
                     <button onClick={this.handleBackButton}>◀</button>
+                    <button onClick={this.handleTeamButton}>T</button>
                 </header>
                 <div className="content">
                     {
                         this.props.mode === Mode.ChooseScenario ? <ChooseScenarioComponent/> :
+                        this.props.mode === Mode.TeamDetails ? <TeamComponent/> :
                         'Error: Unknown mode.'
                     }
                 </div>
