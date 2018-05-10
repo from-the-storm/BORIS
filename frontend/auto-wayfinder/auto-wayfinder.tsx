471893197f6fa35d065b9b947f2d063680c6db22
Braden MacDonald <mail@bradenm.com>
2018-05-09 17:39:12 -0700

    Improve formatting of styles

--- frontend/auto-wayfinder/auto-wayfinder.tsx
+++ frontend/auto-wayfinder/auto-wayfinder.tsx
@@ -28,7 +28,18 @@ export class AutoWayfinder extends React.Component<Props> {
                     options={{
                         zoomControl: false,
                         ...(this.props.lost ? {gestureHandling: 'none'} : {gestureHandling: 'cooperative'}),
-                        styles: [{"featureType":"administrative","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":"40"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"lightness":-25},{"saturation":-100}]}]
+                        styles: [
+                            {featureType: 'administrative', elementType: "all", stylers: [{"saturation":"-100"}]},
+                            {featureType: 'administrative.province', elementType: "all", stylers: [{"visibility":"off"}]},
+                            {featureType: 'landscape', elementType: "all", stylers: [{"saturation":-100},{"lightness":65},{"visibility":"on"}]},
+                            {featureType: 'poi', elementType: "all", stylers: [{"visibility":"off"}]},
+                            {featureType: 'road', elementType: "all", stylers: [{"saturation":"-100"}]},
+                            {featureType: 'road.highway', elementType: "all", stylers: [{"visibility":"simplified"}]},
+                            {featureType: 'road.arterial', elementType: "all", stylers: [{"lightness":"30"}]},
+                            {featureType: 'road.local', elementType: "all", stylers: [{"lightness":"40"}]},
+                            {featureType: 'transit', elementType: "all", stylers: [{"saturation":-100},{"visibility":"simplified"}]},
+                            {featureType: 'water', elementType: "geometry", stylers: [{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},
+                            {featureType: 'water', elementType: "labels", stylers: [{"lightness":-25},{"saturation":-100}]}]
                     }}
                 >
                     {this.props.lost ? (
