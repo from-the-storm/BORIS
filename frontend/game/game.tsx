59bc0a0ed4cb0f76d4cec10849fd1dcdd999126d
Braden MacDonald <mail@bradenm.com>
2018-07-06 21:31:36 -0700

    Remove the hard-coded demo steps from the game

--- frontend/game/game.tsx
+++ frontend/game/game.tsx
@@ -59,69 +59,6 @@ class _GameComponent extends React.PureComponent<Props, State> {
                 </header>
                 <div className="content">
                     {uiElements}
-                    <div className="chat-segment">
-                        <br/><br/><br/><br/><br/><br/><br/><br/>
-                    </div>
-                    <div className="chat-segment">
-                        <p>It's always better on holiday.</p>
-                        <p>So much better on holiday.</p>
-                        <p>That's why we only work when we need the money.</p>
-                    </div>
-                    <div className="chat-segment backfeed">
-                        <p>How far do we have to stretch the truth to fit the lifestyles borrowed and overdue?</p>
-                    </div>
-                    <div className="response-segment">
-                        <button>Halfway there</button>
-                        <button>You said survive alone</button>
-                    </div>
-                    <div className="chat-segment clarence">
-                        <p>Funny how music put times in perspective</p>
-                        <p>Add a sountrack to your life and perfect it. Whenever you are feeling blue keep walking and we can get far.</p>
-                    </div>
-                    <div className="response-segment">
-                        <button className="wrong">Where's he get it all from?</button>
-                        <button>This hell of a temper</button>
-                        <button className="correct">It's so good to see you again</button>
-                        <button>It's beautiful when he talks to you even though we can't really tell what he's saying</button>
-                    </div>
-                    <div className="chat-segment">
-                        <p>That's right. <span className="saltines gained">+10</span> saltines for your team. Or sorry, wait. No <span className="saltines lost">-2</span> saltines for your team.</p>
-                    </div>
-                    <div className="no-segment">
-                        <p>Hello these two kids and they've gotta make an ungodly decision. They decide which one gets to leave.</p>
-                        <p>And which one gets to make it, forsake it. All that I know is no way to fix it.</p>
-                        <p>Quit watching me sleep.</p>
-                    </div>
-                    <div className="chat-segment">
-                        <p>Hello these two kids and they've gotta make an ungodly decision. They decide which one gets to leave.</p>
-                        <p>And which one gets to make it, forsake it. All that I know is no way to fix it.</p>
-                    </div>
-                    <div className="bulletin-segment">
-                        <p>Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. </p>
-                    </div>
-                    <div className="chat-segment">
-                        <p>When the first officer arrived...</p>
-                    </div>
-                    <div className="response-segment">
-                        <form>
-                            <input className="deselected" required type="text" placeholder="..." aria-label="Response to Question" />
-                            <button>Submit</button>
-                        </form>
-                    </div>
-                    <div className="no-segment">
-                        <p>Hello these two kids and they've gotta make an ungodly decision. They decide which one gets to leave.</p>
-                        <p>And which one gets to make it, forsake it. All that I know is no way to fix it.</p>
-                        <p>Quit watching me sleep.</p>
-                    </div>
-                    <div className="response-segment">
-                        <form>
-                            <textarea rows={4} cols={30} className="deselected" required placeholder="..." aria-label="Longer response to question"></textarea>
-                            <button>Submit</button>
-                        </form>
-                    </div>
-                    <div className="bulletin-segment">
-                        <p>Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. Somebody said it's unspeakable. You lift that burden off of me. </p>
-                    </div>
                 </div>
             </div>
             <Prompt close={this.handleCancelQuitPrompt}
