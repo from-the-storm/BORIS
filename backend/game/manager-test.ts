96cbd9e4af92f640e852fd48159d51fe663a28f3
Braden MacDonald <mail@bradenm.com>
2018-08-09 12:13:19 -0700

    Placeholder test for issue where game freezes and can't be abandoned

--- backend/game/manager-test.ts
+++ backend/game/manager-test.ts
@@ -14,147 +14,172 @@ describe("GameManager tests", () => {
     let db: BorisDatabase;
     let teamId: number;
     let userId1: number;
-    let gameManager: GameManager;
     beforeAll(async () => { db = await getDB(); });
     afterAll(async () => { await db.instance.$pool.end(); });
     beforeEach(async () => {
         const data = await createTeam(db, 3);
         teamId = data.teamId;
         userId1 = data.user1.id;
-        const testContext = {db, publishEventToUsers: () => {}};
-        gameManager = (await GameManager.startGame(teamId, TEST_SCENARIO_ID, testContext)).manager;
-    });
-    afterEach(async () => {
-        if (gameManager.status === GameStatus.InProgress) {
-            await gameManager.abandon();
-        }
     });
 
-    describe("GameVars", () => {
-
-        describe("Team Scope", () => {
-
-            it("Returns the default value when it hasn't been set", async () => {
-                expect(gameManager.getVar(teamVarNumber)).toEqual(teamVarNumber.default);
-            });
-
-            it("Returns the new value when it gets set", async () => {
-                const setResult = await gameManager.setVar(teamVarNumber, val => 42);
-                expect(setResult).toEqual(42);
-                expect(gameManager.getVar(teamVarNumber)).toEqual(42);
-                await gameManager.setVar(teamVarNumber, val => ++val);
-                expect(gameManager.getVar(teamVarNumber)).toEqual(43);
-            });
-
-            it("Does not permanently affect the team vars if the game is abandoned", async() => {
-                expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(10); // Default value
-                await gameManager.setVar(teamVarNumber, val => 42);
-                expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(42);
+    describe("using the Test Scenario", () => {
+        let gameManager: GameManager;
+        beforeEach(async () => {
+            const testContext = {db, publishEventToUsers: () => {}};
+            gameManager = (await GameManager.startGame(teamId, TEST_SCENARIO_ID, testContext)).manager;
+        });
+        afterEach(async () => {
+            if (gameManager.status === GameStatus.InProgress) {
                 await gameManager.abandon();
-                expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(10);
+            }
+        });
+
+        describe("GameVars", () => {
+
+            describe("Team Scope", () => {
+
+                it("Returns the default value when it hasn't been set", async () => {
+                    expect(gameManager.getVar(teamVarNumber)).toEqual(teamVarNumber.default);
+                });
+
+                it("Returns the new value when it gets set", async () => {
+                    const setResult = await gameManager.setVar(teamVarNumber, val => 42);
+                    expect(setResult).toEqual(42);
+                    expect(gameManager.getVar(teamVarNumber)).toEqual(42);
+                    await gameManager.setVar(teamVarNumber, val => ++val);
+                    expect(gameManager.getVar(teamVarNumber)).toEqual(43);
+                });
+
+                it("Does not permanently affect the team vars if the game is abandoned", async() => {
+                    expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(10); // Default value
+                    await gameManager.setVar(teamVarNumber, val => 42);
+                    expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(42);
+                    await gameManager.abandon();
+                    expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(10);
+                });
+
+                it("Permanently affects the team vars if the game completes successfully", async() => {
+                    expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(10); // Default value
+                    await gameManager.setVar(teamVarNumber, val => 42);
+                    expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(42);
+                    // Finish the test game:
+                    await gameManager.allPendingStepsFlushed();
+                    const lastStepId = Array.from(gameManager.steps.keys()).pop();
+                    await gameManager.callStepHandler({stepId: lastStepId, choiceId: 'x'});
+                    await gameManager.allPendingStepsFlushed();
+                    expect(gameManager.status).toBe(GameStatus.InReview);
+                    //////
+                    expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(42);
+                    // Note: the new value '42' will affect any subsequent test cases here.
+                });
+
             });
 
-            it("Permanently affects the team vars if the game completes successfully", async() => {
-                expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(10); // Default value
-                await gameManager.setVar(teamVarNumber, val => 42);
-                expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(42);
-                // Finish the test game:
-                await gameManager.allPendingStepsFlushed();
-                const lastStepId = Array.from(gameManager.steps.keys()).pop();
-                await gameManager.callStepHandler({stepId: lastStepId, choiceId: 'x'});
-                await gameManager.allPendingStepsFlushed();
-                expect(gameManager.status).toBe(GameStatus.InReview);
-                //////
-                expect(await getTeamVar(teamVarNumber, teamId, db)).toEqual(42);
-                // Note: the new value '42' will affect any subsequent test cases here.
+            describe("Game Scope", () => {
+
+                it("Returns the default value when it hasn't been set", async () => {
+                    expect(gameManager.getVar(gameVarNumber)).toEqual(gameVarNumber.default);
+                });
+
+                it("Returns the new value when it gets set", async () => {
+                    const setResult = await gameManager.setVar(gameVarNumber, val => 42);
+                    expect(setResult).toEqual(42);
+                    expect(gameManager.getVar(gameVarNumber)).toEqual(42);
+                    await gameManager.setVar(gameVarNumber, val => ++val);
+                    expect(gameManager.getVar(gameVarNumber)).toEqual(43);
+                });
+
+            });
+
+            describe("Step Scope", () => {
+
+                it("Returns the default value when it hasn't been set", async () => {
+                    expect(gameManager.getVar(stepVarString, 1)).toEqual(stepVarString.default);
+                });
+
+                it("Is scoped to a step ID", async () => {
+                    const setResult = await gameManager.setVar(stepVarString, val => "Bob", 1);
+                    expect(setResult).toEqual("Bob");
+                    await gameManager.setVar(stepVarString, val => "Axe", 5);
+                    expect(gameManager.getVar(stepVarString, 1)).toEqual("Bob");
+                    expect(gameManager.getVar(stepVarString, 2)).toEqual("");
+                    expect(gameManager.getVar(stepVarString, 5)).toEqual("Axe");
+                });
+
+            });
+
+            describe("In JS Expressions", () => {
+                it("Gets 'undefined' when requesting a variable that's not set", () => {
+                    expect(gameManager.safeEvalScriptExpression("VAR('foobar')", userId1)).toBe(undefined);
+                });
+                it("Can do conditionals on team vars", async () => {
+                    await gameManager.setVar(teamVarNumber, val => 123456);
+                    expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') === 123456", userId1)).toBe(true);
+                    expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') === 567890", userId1)).toBe(false);
+                    expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') > 120000", userId1)).toBe(true);
+                    expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') > 567890", userId1)).toBe(false);
+                });
+                it("Can do conditionals on game vars", async () => {
+                    await gameManager.setVar(gameVarNumber, val => 500);
+                    expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') === 500", userId1)).toBe(true);
+                    expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') === 200", userId1)).toBe(false);
+                    expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') > 250", userId1)).toBe(true);
+                    expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') > 600", userId1)).toBe(false);
+                });
             });
 
         });
 
-        describe("Game Scope", () => {
-
-            it("Returns the default value when it hasn't been set", async () => {
-                expect(gameManager.getVar(gameVarNumber)).toEqual(gameVarNumber.default);
+        describe("safeEvalScriptExpression", () => {
+            it("Can do basic calculations", () => {
+                expect(gameManager.safeEvalScriptExpression("1 + 1", userId1)).toBe(2);
+                expect(gameManager.safeEvalScriptExpression("true === true", userId1)).toBe(true);
+                expect(gameManager.safeEvalScriptExpression("'hello' === 'goobye'", userId1)).toBe(false);
             });
-
-            it("Returns the new value when it gets set", async () => {
-                const setResult = await gameManager.setVar(gameVarNumber, val => 42);
-                expect(setResult).toEqual(42);
-                expect(gameManager.getVar(gameVarNumber)).toEqual(42);
-                await gameManager.setVar(gameVarNumber, val => ++val);
-                expect(gameManager.getVar(gameVarNumber)).toEqual(43);
-            });
-
-        });
-
-        describe("Step Scope", () => {
-
-            it("Returns the default value when it hasn't been set", async () => {
-                expect(gameManager.getVar(stepVarString, 1)).toEqual(stepVarString.default);
-            });
-
-            it("Is scoped to a step ID", async () => {
-                const setResult = await gameManager.setVar(stepVarString, val => "Bob", 1);
-                expect(setResult).toEqual("Bob");
-                await gameManager.setVar(stepVarString, val => "Axe", 5);
-                expect(gameManager.getVar(stepVarString, 1)).toEqual("Bob");
-                expect(gameManager.getVar(stepVarString, 2)).toEqual("");
-                expect(gameManager.getVar(stepVarString, 5)).toEqual("Axe");
-            });
-
-        });
-
-        describe("In JS Expressions", () => {
-            it("Gets 'undefined' when requesting a variable that's not set", () => {
-                expect(gameManager.safeEvalScriptExpression("VAR('foobar')", userId1)).toBe(undefined);
-            });
-            it("Can do conditionals on team vars", async () => {
-                await gameManager.setVar(teamVarNumber, val => 123456);
-                expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') === 123456", userId1)).toBe(true);
-                expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') === 567890", userId1)).toBe(false);
-                expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') > 120000", userId1)).toBe(true);
-                expect(gameManager.safeEvalScriptExpression("VAR('teamVarNumber') > 567890", userId1)).toBe(false);
-            });
-            it("Can do conditionals on game vars", async () => {
-                await gameManager.setVar(gameVarNumber, val => 500);
-                expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') === 500", userId1)).toBe(true);
-                expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') === 200", userId1)).toBe(false);
-                expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') > 250", userId1)).toBe(true);
-                expect(gameManager.safeEvalScriptExpression("VAR('gameVarNumber') > 600", userId1)).toBe(false);
+            it("Can access player survey data using USER_INFO()", () => {
+                expect(gameManager.safeEvalScriptExpression("USER_INFO().survey_data.q1", userId1)).toEqual('a1');
             });
         });
 
+        describe("getElapsedTime()", () => {
+            it("is being tested on a computer whose clock is in sync with the DB's clock", async () => {
+                const dbResults = await db.query('SELECT NOW() as nowdate', [], {});
+                const dbDate = dbResults[0].nowdate;
+                const nowDate = new Date();
+                expect(+nowDate - +dbDate).toBeLessThan(3000);
+                expect(+nowDate - +dbDate).toBeGreaterThan(-3000);
+            });
+            it("Returns a value that seems reasonable", async () => {
+                await new Promise (r => setTimeout(r, 1000));
+                const origTime = gameManager.getElapsedTime();
+                expect(origTime).toBeGreaterThanOrEqual(1.0);
+                expect(origTime).toBeLessThan(30);
+                await new Promise (r => setTimeout(r, 1000));
+                const newTime = gameManager.getElapsedTime();
+                expect(newTime - origTime).toBeGreaterThanOrEqual(1.0);
+                expect(newTime - origTime).toBeLessThan(10.0);
+            });
+        });
     });
 
-    describe("safeEvalScriptExpression", () => {
-        it("Can do basic calculations", () => {
-            expect(gameManager.safeEvalScriptExpression("1 + 1", userId1)).toBe(2);
-            expect(gameManager.safeEvalScriptExpression("true === true", userId1)).toBe(true);
-            expect(gameManager.safeEvalScriptExpression("'hello' === 'goobye'", userId1)).toBe(false);
-        });
-        it("Can access player survey data using USER_INFO()", () => {
-            expect(gameManager.safeEvalScriptExpression("USER_INFO().survey_data.q1", userId1)).toEqual('a1');
+    describe("abandon() tests", () => {
+        it("Does not get 'stuck' if scripts run long", async () => {
+            const testContext = {db, publishEventToUsers: () => {}};
+
+            // Create a script that pauses for 200 seconds, and a scenario
+            // that uses it.
+            const longRunningScript = await db.scripts.insert({
+                name: "long-running-script",
+                script_yaml: `- step: pause\n  for: 200\n- step: pause\n  for: 0\n  if: "while (1) {}"`,
+            });
+            const longRunningScenario = await db.scenarios.insert({
+                name: "Long Running Scenario",
+                is_active: true,
+                script: longRunningScript.name,
+            });
+            const gameManager = (await GameManager.startGame(teamId, longRunningScenario.id, testContext)).manager;
+
+            await gameManager.abandon();
         });
     });
-
-    describe("getElapsedTime()", () => {
-        it("is being tested on a computer whose clock is in sync with the DB's clock", async () => {
-            const dbResults = await db.query('SELECT NOW() as nowdate', [], {});
-            const dbDate = dbResults[0].nowdate;
-            const nowDate = new Date();
-            expect(+nowDate - +dbDate).toBeLessThan(3000);
-            expect(+nowDate - +dbDate).toBeGreaterThan(-3000);
-        });
-        it("Returns a value that seems reasonable", async () => {
-            await new Promise (r => setTimeout(r, 1000));
-            const origTime = gameManager.getElapsedTime();
-            expect(origTime).toBeGreaterThanOrEqual(1.0);
-            expect(origTime).toBeLessThan(30);
-            await new Promise (r => setTimeout(r, 1000));
-            const newTime = gameManager.getElapsedTime();
-            expect(newTime - origTime).toBeGreaterThanOrEqual(1.0);
-            expect(newTime - origTime).toBeLessThan(10.0);
-        });
-    });
-
 });
